90a57dcb63b8f7807d71af56d4bd72aa6ffff653
Devashish2606 <smartdebu06@gmail.com>
2026-09-08 10:09:35 +0530

    add helper method

--- lab3/App.js
+++ lab3/App.js
@@ -1,8 +1,31 @@
 import http from "http";
+//import * as teams from 'team.js'
+import { getAllTeams } from  "./team.js";
 
-const server = http.createServer((req, res) =>{
-    res.end("<h2> SIH Internal <h2>");
-});
+
+const PORT = 5000;
+
+const sendJson = (res,statusCode, data) => {
+    res.writeHead(statusCode,{"content-type": "application/json"});
+    res.end(data === "undefined" ? "" : json.stringify(data));
+};
+
+const parseJSONBody = (req)=>{
+    new Promise((reqsolve, reject) => {
+        letbody="";
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        req.on("end", () => {
+            try {
+                resolve(body? JSON.parse(body) : {});
+            } catch (error){
+                reject(error);
+            }
+        });
+    });
+};
+const server = http.createServer((req, res) =>{});
 
 /*server.on('request', (req,res)=>{
     res.write("<h1>Welcome to Server Side Programming<h1>");
@@ -10,8 +33,8 @@ const server = http.createServer((req, res) =>{
     res.end();
 });*/
 
-server.listen(5000, () =>{
-    console.log("Server is running");
-})
+server.listen(PORT, () =>{
+    console.log("SIH Server is running", PORT);
+});
 
 //Ctrl+C to terminate the server
